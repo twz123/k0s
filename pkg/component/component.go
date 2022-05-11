c9837e4f6130c42daceb23c586a208aba26380e6
Tom Wieczorek <twieczorek@mirantis.com>
2022-05-11 09:00:29 +0200

    WIP

--- pkg/component/component.go
+++ pkg/component/component.go
@@ -17,6 +17,8 @@ package component
 
 import (
 	"context"
+	"errors"
+	"sync"
 
 	"github.com/k0sproject/k0s/pkg/apis/k0s.k0sproject.io/v1beta1"
 )
@@ -72,3 +74,303 @@ type ReconcilerComponent interface {
 	// configuration. Reconcile may only be called after Init and before Stop.
 	Reconcile(context.Context, *v1beta1.ClusterConfig) error
 }
+
+var ErrNotYetInitialized = errors.New("not yet initialized")
+var ErrAlreadyInitialized = errors.New("already initialized")
+var ErrNotYetRunning = errors.New("not yet running")
+var ErrAlreadyRunning = errors.New("already running")
+var ErrAlreadyStopped = errors.New("already stopped")
+
+type ComponentSkeleton[C, I, R any] struct {
+	Initialize        func(context.Context, C, *v1beta1.ClusterSpec) (I, error)
+	Run               func(context.Context, I, *v1beta1.ClusterSpec) (R, error)
+	Reconcile         func(context.Context, R, *v1beta1.ClusterSpec) error
+	Healthy           func(R) error
+	StopAfterCreation func(C) error
+	StopAfterInit     func(I) error
+	stopWhenRunning   func(R) error
+}
+
+func (skel ComponentSkeleton[C, I, R]) Create(state C) Component {
+	if skel.Reconcile == nil {
+		return &component[C, I, R]{
+			inner: createdComponent[C, I, R]{
+				state,
+				skel.Initialize,
+				skel.Run,
+				skel.Healthy,
+				skel.StopAfterCreation,
+				skel.StopAfterInit,
+				skel.stopWhenRunning,
+			},
+		}
+	}
+
+	return &reconcileComponent[C, I, R]{
+		component[C, I, R]{
+			inner: createdComponent[C, I, R]{
+				state,
+				skel.Initialize,
+				skel.Run,
+				skel.Healthy,
+				skel.StopAfterCreation,
+				skel.StopAfterInit,
+				skel.stopWhenRunning,
+			},
+		},
+		skel.Reconcile,
+	}
+}
+
+type component[C, I, R any] struct {
+	mu    sync.Mutex
+	inner any
+}
+
+type reconcileComponent[C, I, R any] struct {
+	component[C, I, R]
+	reconcile func(context.Context, R, *v1beta1.ClusterSpec) error
+}
+
+func (c *reconcileComponent[C, I, R]) Reconcile(ctx context.Context, spec *v1beta1.ClusterSpec) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	switch inner := c.inner.(type) {
+	case createdComponent[C, I, R]:
+		return ErrNotYetRunning
+
+	case initializedComponent[I, R]:
+		return ErrNotYetRunning
+
+	case runningComponent[R]:
+		return c.reconcile(ctx, inner.state, spec)
+
+	default:
+		return ErrAlreadyStopped
+	}
+}
+
+type createdComponent[C, I, R any] struct {
+	state C
+
+	initialize        func(context.Context, C, *v1beta1.ClusterSpec) (I, error)
+	run               func(context.Context, I, *v1beta1.ClusterSpec) (R, error)
+	healthy           func(R) error
+	stopAfterCreation func(C) error
+	stopAfterInit     func(I) error
+	stopWhenRunning   func(R) error
+}
+
+type initializedComponent[I, R any] struct {
+	state I
+
+	run             func(context.Context, I, *v1beta1.ClusterSpec) (R, error)
+	healthy         func(R) error
+	stopAfterInit   func(I) error
+	stopWhenRunning func(R) error
+}
+
+type runningComponent[R any] struct {
+	state R
+
+	healthy         func(R) error
+	stopWhenRunning func(R) error
+}
+
+func (c *component[C, I, R]) Init(ctx context.Context) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var spec *v1beta1.ClusterSpec
+	if theSpec, ok := ctx.Value("clusterSpec").(*v1beta1.ClusterSpec); ok {
+		spec = theSpec
+	}
+
+	switch inner := c.inner.(type) {
+	case createdComponent[C, I, R]:
+		var initialized I
+
+		if inner.initialize != nil {
+			var err error
+			initialized, err = inner.initialize(ctx, inner.state, spec)
+			if err != nil {
+				return err
+			}
+		}
+
+		c.inner = initializedComponent[I, R]{
+			initialized,
+			inner.run,
+			inner.healthy,
+			inner.stopAfterInit,
+			inner.stopWhenRunning,
+		}
+
+		return nil
+
+	case initializedComponent[I, R]:
+		return ErrAlreadyInitialized
+
+	case runningComponent[R]:
+		return ErrAlreadyRunning
+
+	default:
+		return ErrAlreadyStopped
+	}
+}
+
+func (c *component[C, I, R]) Run(ctx context.Context) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var spec *v1beta1.ClusterSpec
+	if theSpec, ok := ctx.Value("clusterSpec").(*v1beta1.ClusterSpec); ok {
+		spec = theSpec
+	}
+
+	switch inner := c.inner.(type) {
+	case createdComponent[C, I, R]:
+		return ErrNotYetInitialized
+
+	case initializedComponent[I, R]:
+		var running R
+
+		if inner.run != nil {
+			var err error
+			running, err = inner.run(ctx, inner.state, spec)
+			if err != nil {
+				return err
+			}
+		}
+
+		c.inner = runningComponent[R]{
+			running,
+			inner.healthy,
+			inner.stopWhenRunning,
+		}
+
+		return nil
+
+	case runningComponent[R]:
+		return ErrAlreadyRunning
+
+	default:
+		return ErrAlreadyStopped
+	}
+}
+
+func (c *component[C, I, R]) Healthy() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	switch state := c.inner.(type) {
+	case createdComponent[C, I, R]:
+		return ErrNotYetInitialized
+
+	case initializedComponent[I, R]:
+		return ErrNotYetRunning
+
+	case runningComponent[R]:
+		if state.healthy != nil {
+			return state.healthy(state.state)
+		}
+		return nil
+
+	default:
+		return ErrAlreadyStopped
+	}
+}
+
+func (c *component[C, I, R]) Stop() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	switch state := c.inner.(type) {
+	case createdComponent[C, I, R]:
+		if state.stopAfterCreation != nil {
+			if err := state.stopAfterCreation(state.state); err != nil {
+				return err
+			}
+		}
+
+		c.inner = nil
+		return nil
+
+	case initializedComponent[I, R]:
+		if state.stopAfterInit != nil {
+			if err := state.stopAfterInit(state.state); err != nil {
+				return err
+			}
+		}
+
+		c.inner = nil
+		return nil
+
+	case runningComponent[R]:
+		if state.stopWhenRunning != nil {
+			if err := state.stopWhenRunning(state.state); err != nil {
+				return err
+			}
+		}
+
+		c.inner = nil
+		return nil
+
+	default:
+		return ErrAlreadyStopped
+	}
+}
+
+// type Uninitialized[I any] interface {
+// 	Init(context.Context) (I, error)
+// }
+
+// func InitializerFunc[I any](fn func(context.Context) (I, error)) Uninitialized[I] {
+// 	tr := newTransitionFunc(fn, func() error { return ErrAlreadyInitialized })
+// 	return initializerFunc[I](tr.transition)
+// }
+
+// type initializerFunc[I any] func(context.Context) (I, error)
+
+// func (f initializerFunc[I]) Init(ctx context.Context) (I, error) {
+// 	return f(ctx)
+// }
+
+// type Initialized[R any] interface {
+// 	Run(context.Context) (R, error)
+// }
+
+// func RunFunc[R any](fn func(context.Context) (R, error)) Initialized[R] {
+// 	tr := newTransitionFunc(fn, func() error { return ErrAlreadyRunning })
+// 	return runFunc[R](tr.transition)
+// }
+
+// type runFunc[R any] func(context.Context) (R, error)
+
+// func (f runFunc[R]) Run(ctx context.Context) (R, error) {
+// 	return f(ctx)
+// }
+
+// type transitionFunc[T any] struct {
+// 	fn                  atomic.Value
+// 	alreadyTransitioned func() error
+// }
+
+// func newTransitionFunc[T any](fn func(context.Context) (T, error), alreadyTransitioned func() error) *transitionFunc[T] {
+// 	f := new(transitionFunc[T])
+// 	f.fn.Store(&fn)
+// 	f.alreadyTransitioned = alreadyTransitioned
+// 	return f
+// }
+
+// func (f *transitionFunc[T]) transition(ctx context.Context) (transitioned T, err error) {
+// 	var consumed func(context.Context) (T, error)
+// 	if fn := f.fn.Swap(&consumed).(*func(context.Context) (T, error)); fn != nil {
+// 		transitioned, err = (*fn)(ctx)
+// 	} else {
+// 		err = f.alreadyTransitioned()
+// 	}
+
+// 	return
+// }
