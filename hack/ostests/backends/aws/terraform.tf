9688b2316da2b79e56604359d53ec8b6272e537d
Tom Wieczorek <twieczorek@mirantis.com>
2023-05-30 10:07:12 +0200

    Add random, as it's used for pet names
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- hack/ostests/backends/aws/terraform.tf
+++ hack/ostests/backends/aws/terraform.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    aws   = { source = "hashicorp/aws", version = "~> 4.0", }
-    local = { source = "hashicorp/local", version = "~> 2.0", }
+    aws    = { source = "hashicorp/aws", version = "~> 4.0", }
+    local  = { source = "hashicorp/local", version = "~> 2.0", }
+    random = { source = "hashicorp/random", version = "~> 3.0", }
   }
 }
