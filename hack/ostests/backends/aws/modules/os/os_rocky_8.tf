5e8cddae279e12f3d03be6d34bc1507358ae8ebe
Tom Wieczorek <twieczorek@mirantis.com>
2023-06-05 10:40:29 +0200

    Add Rocky 8
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

---
+++ hack/ostests/backends/aws/modules/os/os_rocky_8.tf
@@ -0,0 +1,40 @@
+# https://rockylinux.org/cloud-images/
+
+data "aws_ami" "rocky_8" {
+  count = var.os == "rocky_8" ? 1 : 0
+
+  owners      = ["792107900819"]
+  name_regex  = "^Rocky-8-EC2-Base-8\\.7-\\d+\\.\\d+\\.x86_64"
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Rocky-8-EC2-Base-8.7-*.x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+locals {
+  os_rocky_8 = merge({
+    id           = var.os
+    ssh_username = "rocky"
+    },
+    var.os != "rocky_8" ? {} : {
+      controller_ami = {
+        id = data.aws_ami.rocky_8.0.id
+      }
+      worker_ami = {
+        id = data.aws_ami.rocky_8.0.id
+      }
+    },
+  )
+}
