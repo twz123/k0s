65bc179533bcdcf182e0d87bdabce0f0bd0291fd
Tom Wieczorek <twieczorek@mirantis.com>
2023-06-05 10:56:35 +0200

    Add Debian 11
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- hack/ostests/README.md
+++ hack/ostests/README.md
@@ -47,6 +47,7 @@ To see runs for this workflow, try: gh run list --workflow=ostests.yaml
 * `centos_7`: CentOS Linux 7 (Core)
 * `centos_8`: CentOS Stream 8
 * `centos_9`: CentOS Stream 9
+* `debian_11`: Debian GNU/Linux 11 (bullseye)
 * `rhel_7`: Red Hat Enterprise Linux Server 7.9 (Maipo)
 * `rhel_8`: Red Hat Enterprise Linux 8.6 (Ootpa)
 * `rhel_9`: Red Hat Enterprise Linux 9.0 (Plow)
---
+++ hack/ostests/backends/aws/modules/os/os_debian_11.tf
@@ -0,0 +1,40 @@
+# https://wiki.debian.org/Cloud/AmazonEC2Image/Bullseye
+
+data "aws_ami" "debian_11" {
+  count = var.os == "debian_11" ? 1 : 0
+
+  owners      = ["136693071363"]
+  name_regex  = "^debian-11-amd64-\\d+-\\d+$"
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["debian-11-amd64-*-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+locals {
+  os_debian_11 = merge({
+    id           = var.os
+    ssh_username = "admin"
+    },
+    var.os != "debian_11" ? {} : {
+      controller_ami = {
+        id = data.aws_ami.debian_11.0.id
+      }
+      worker_ami = {
+        id = data.aws_ami.debian_11.0.id
+      }
+    },
+  )
+}
