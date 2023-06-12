6fe3bd0b933fc764ee3676a83c586e2bb5532ecc
Tom Wieczorek <twieczorek@mirantis.com>
2023-06-12 14:53:21 +0200

    Remove libvirt for the time being
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- hack/ostests/backends/libvirt/modules/backend/modules/machine/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 1.4.0"
-
-  required_providers {
-    libvirt = { source = "dmacvicar/libvirt", version = "~> 0.7.0", }
-    random  = { source = "hashicorp/random", version = "~> 3.0", }
-  }
-}
--- hack/ostests/backends/libvirt/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = ">= 1.4.0"
-
-  required_providers {
-    libvirt = { source = "dmacvicar/libvirt", version = "~> 0.7.0", }
-    local   = { source = "hashicorp/local", version = "~> 2.0", }
-  }
-}
