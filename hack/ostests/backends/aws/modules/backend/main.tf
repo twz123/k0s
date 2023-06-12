a84131774c38f0f561219903f5ea1f668d21864a
Tom Wieczorek <twieczorek@mirantis.com>
2023-06-12 14:48:45 +0200

    Refactor machine array
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- hack/ostests/backends/aws/modules/backend/main.tf
+++ hack/ostests/backends/aws/modules/backend/main.tf
@@ -12,50 +12,29 @@ locals {
     instance_type = "t3a.small"
   }
 
-  node_configs = {
-    controller          = merge(local.default_node_config, var.os.node_configs.default, var.os.node_configs.controller)
-    "controller+worker" = merge(local.default_node_config, var.os.node_configs.default, var.os.node_configs.worker, var.os.node_configs.controller_worker)
-    worker              = merge(local.default_node_config, var.os.node_configs.default, var.os.node_configs.worker)
+  machine_roles = {
+    "controller" : {
+      count       = var.controller_num_nodes
+      node_config = merge(local.default_node_config, var.os.node_configs.default, var.os.node_configs.controller)
+    }
+
+    "controller+worker" = {
+      count       = var.controller_worker_num_nodes
+      node_config = merge(local.default_node_config, var.os.node_configs.default, var.os.node_configs.worker, var.os.node_configs.controller_worker)
+    }
+
+    "worker" = {
+      count       = var.worker_num_nodes
+      node_config = merge(local.default_node_config, var.os.node_configs.default, var.os.node_configs.worker)
+    }
   }
-}
 
-resource "terraform_data" "controllers" {
-  count = var.controller_num_nodes
-
-  input = {
-    name        = format("%s-controller-%d", var.resource_name_prefix, count.index)
-    role        = "controller"
-    node_config = local.node_configs.controller
-  }
-}
-
-resource "terraform_data" "controller_workers" {
-  count = var.controller_worker_num_nodes
-
-  input = {
-    name        = format("%s-controller-%d", var.resource_name_prefix, var.controller_num_nodes + count.index)
-    role        = "controller+worker"
-    node_config = local.node_configs["controller+worker"]
-  }
-}
-
-resource "terraform_data" "workers" {
-  count = var.worker_num_nodes
-
-  input = {
-    name        = format("%s-worker-%d", var.resource_name_prefix, count.index)
-    role        = "worker"
-    node_config = local.node_configs.worker
-  }
-}
-
-locals {
-  # FIXME this is not suitable when scaling out controllers or controller+workers
-  machines = { for idx, machine in concat(
-    terraform_data.controllers.*.output,
-    terraform_data.controller_workers.*.output,
-    terraform_data.workers.*.output
-  ) : "machine-${idx}" => machine }
+  machines = merge([for role, params in local.machine_roles : {
+    for idx in range(params.count) : "${role}-${idx + 1}" => {
+      role        = role
+      node_config = params.node_config
+    }
+  }]...)
 }
 
 resource "aws_instance" "machines" {
@@ -68,8 +47,9 @@ resource "aws_instance" "machines" {
   user_data = each.value.node_config.user_data
 
   tags = {
-    Name                             = each.value.name
-    "k0sctl.k0sproject.io/host-role" = each.value.role
+    Name                              = "${var.resource_name_prefix}-${each.key}"
+    "ostests.k0sproject.io/node-name" = each.key
+    "k0sctl.k0sproject.io/host-role"  = each.value.role
   }
 
   key_name                    = aws_key_pair.ssh.key_name
@@ -87,22 +67,18 @@ resource "aws_instance" "machines" {
 }
 
 resource "terraform_data" "ready_scripts" {
-  for_each = { for idx, machine in concat(
-    local.node_configs.controller.ready_script == null ? [] : terraform_data.controllers.*.output,
-    local.node_configs["controller+worker"].ready_script == null ? [] : terraform_data.controller_workers.*.output,
-    local.node_configs.worker.ready_script == null ? [] : terraform_data.workers.*.output,
-  ) : "script-${idx}" => machine }
+  for_each = { for name, params in local.machines : name => params if params.node_config.ready_script != null }
 
   connection {
     type        = each.value.node_config.connection.type
     user        = each.value.node_config.connection.username
     private_key = tls_private_key.ssh.private_key_pem
-    host        = one([for i in aws_instance.machines : i if i.tags.Name == each.value.name]).public_ip
+    host        = aws_instance.machines[each.key].public_ip
     agent       = false
   }
 
   provisioner "remote-exec" {
-    inline = each.value.node_config.ready_script != null ? [each.value.node_config.ready_script] : []
+    inline = [each.value.node_config.ready_script]
   }
 }
 
@@ -113,6 +89,6 @@ resource "terraform_data" "provisioned_machines" {
     name       = machine.tags.Name,
     ipv4       = machine.public_ip,
     role       = machine.tags["k0sctl.k0sproject.io/host-role"]
-    connection = one([for m in local.machines : m if m.name == machine.tags.Name]).node_config.connection
+    connection = local.machines[machine.tags["ostests.k0sproject.io/node-name"]].node_config.connection
   }]
 }
