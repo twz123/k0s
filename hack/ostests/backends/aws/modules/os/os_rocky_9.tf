4f39dca4e3f2127e58230095bbf7c6879972964b
Tom Wieczorek <twieczorek@mirantis.com>
2023-06-13 13:48:31 +0200

    More CloudWatch
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- hack/ostests/backends/aws/modules/os/os_rocky_9.tf
+++ hack/ostests/backends/aws/modules/os/os_rocky_9.tf
@@ -34,6 +34,23 @@ locals {
       default = {
         ami_id = one(data.aws_ami.rocky_9.*.id)
 
+        # https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/install-CloudWatch-Agent-commandline-fleet.html
+        user_data = var.cloudwatch_agent_config == null ? null : format("#cloud-config\n%s", jsonencode({
+          write_files = [{
+            path    = "/etc/k0s-ostests/cloudwatch-agent.json"
+            content = jsonencode(var.cloudwatch_agent_config)
+          }]
+
+          runcmd = [
+            # There's no specific RPM for Rocky. Go on and use Red Hat.
+            "curl -sSLo amazon-cloudwatch-agent.rpm https://s3.amazonaws.com/amazoncloudwatch-agent/redhat/amd64/latest/amazon-cloudwatch-agent.rpm",
+            "rpm -U ./amazon-cloudwatch-agent.rpm",
+            "rm ./amazon-cloudwatch-agent.rpm",
+            "/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/etc/k0s-ostests/cloudwatch-agent.json",
+            "/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start",
+          ]
+        })),
+
         connection = {
           type     = "ssh"
           username = "rocky"
