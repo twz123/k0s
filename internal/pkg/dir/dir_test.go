64f4c1e5278976c5cb30e38b1ffbf3b46797948f
Tom Wieczorek <twieczorek@mirantis.com>
2023-11-30 16:38:31 +0100

    Add dir.Exists as an error-aware alternative to dir.IsDirectory
    The dir.IsDirectory function is potentially hiding errors from callers.
    In some cases, it's good to have a definitive answer to the question
    if a directory really exists or not, or if it couldn't be determined
    because of some OS error. The new function dir.Exists allows for that
    distinction.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- internal/pkg/dir/dir_test.go
+++ internal/pkg/dir/dir_test.go
@@ -19,11 +19,92 @@ package dir_test
 import (
 	"os"
 	"path/filepath"
+	"strings"
+	"syscall"
 	"testing"
 
 	"github.com/k0sproject/k0s/internal/pkg/dir"
+	"github.com/k0sproject/k0s/internal/testutil"
+
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestExists(t *testing.T) {
+	t.Run("existing_directory", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		exists, err := dir.Exists(tmpDir)
+		if assert.NoError(t, err) {
+			assert.True(t, exists, "Freshly created temp dir should exist")
+		}
+	})
+
+	t.Run("nonexistent_path", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		p := filepath.Join(tmpDir, "some-path")
+
+		exists, err := dir.Exists(p)
+		if assert.NoError(t, err) {
+			assert.False(t, exists, "Nonexistent path shouldn't exist")
+		}
+	})
+
+	t.Run("empty_path", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		defer testutil.Chdir(t, tmpDir)()
+		_, err := dir.Exists("")
+		assert.ErrorIs(t, err, os.ErrNotExist)
+		assert.ErrorContains(t, err, "try a dot instead of an empty path")
+
+		exists, err := dir.Exists(".")
+		if assert.NoError(t, err) {
+			assert.True(t, exists, "Current directory should exist")
+		}
+	})
+
+	t.Run("obstructed_path", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		p := filepath.Join(tmpDir, "some-path")
+		require.NoError(t, os.WriteFile(p, []byte("obstructed"), 0644))
+
+		exists, err := dir.Exists(p)
+		if assert.NoError(t, err) {
+			assert.False(t, exists, "Obstructed path shouldn't exist")
+		}
+
+		exists, err = dir.Exists(filepath.Join(p, "sub-path"))
+		if assert.NoError(t, err) {
+			assert.False(t, exists, "Obstructed sub-path shouldn't exist")
+		}
+	})
+
+	t.Run("dangling_symlink", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		nonexistent := filepath.Join(tmpDir, "nonexistent")
+		dangling := filepath.Join(tmpDir, "dangling")
+		require.NoError(t, os.Symlink(nonexistent, dangling))
+
+		exists, err := dir.Exists(dangling)
+		if assert.NoError(t, err) {
+			assert.False(t, exists, "Dangling symlink shouldn't exist")
+		}
+
+		require.NoError(t, os.Mkdir(nonexistent, 0755))
+
+		exists, err = dir.Exists(dangling)
+		if assert.NoError(t, err) {
+			assert.True(t, exists, "Symlinked directory should exist")
+		}
+	})
+
+	t.Run("long_path", func(t *testing.T) {
+		tmpDir := t.TempDir()
+		// The upper limit is 255 for virtually all OSes/FSes
+		longPath := strings.Repeat("x", 256)
+		_, err := dir.Exists(filepath.Join(tmpDir, longPath))
+		assert.ErrorIs(t, err, syscall.ENAMETOOLONG)
+	})
+}
 
 func TestGetAll(t *testing.T) {
 	t.Run("empty", func(t *testing.T) {
