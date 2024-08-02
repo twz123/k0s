d8255653e0d20a28bd0806aa526a5673c4d476da
Tom Wieczorek <twieczorek@mirantis.com>
2024-08-02 13:56:00 +0200

    Rewrite the kubeconfig create test
    Instead of duplicating the behavior of the acutal functionality to be
    tested, and merely just checking if the kubeconfig template is correct,
    make this a real test by actually invoking the Cobra command.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- cmd/kubeconfig/create.go
+++ cmd/kubeconfig/create.go
@@ -98,6 +98,8 @@ Note: A certificate once signed cannot be revoked for a particular user`,
 			return err
 		},
 	}
+
+	cmd.Flags().AddFlagSet(config.FileInputFlag())
 	cmd.Flags().StringVar(&groups, "groups", "", "Specify groups")
 	cmd.PersistentFlags().AddFlagSet(config.GetPersistentFlagSet())
 	return cmd
