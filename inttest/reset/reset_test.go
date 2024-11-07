351357b0f6b3135a8ff19ab08711b99459413b5e
Tom Wieczorek <twieczorek@mirantis.com>
2024-11-07 17:52:48 +0100

    Augment k0s reset integration tests
    Add a script to the reset integration test that tries to clutter the
    data directory by placing files and directories with odd permissions,
    adding symlinks to stuff outside the data directory, and adding bind
    mounts in various ways.
    
    This is to prove that k0's reset will never delete anything that is not
    beneath the data directory.
    
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- inttest/reset/reset_test.go
+++ inttest/reset/reset_test.go
@@ -17,6 +17,11 @@ limitations under the License.
 package reset
 
 import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+	"strings"
 	"testing"
 
 	testifysuite "github.com/stretchr/testify/suite"
@@ -28,11 +33,14 @@ type suite struct {
 	common.BootlooseSuite
 }
 
+//go:embed clutter-data-dir.sh
+var clutterScript []byte
+
 func (s *suite) TestReset() {
 	ctx := s.Context()
 	workerNode := s.WorkerNode(0)
 
-	if ok := s.Run("k0s gets up", func() {
+	if !s.Run("k0s gets up", func() {
 		s.Require().NoError(s.InitController(0, "--disable-components=konnectivity-server,metrics-server"))
 		s.Require().NoError(s.RunWorkers())
 
@@ -44,11 +52,7 @@ func (s *suite) TestReset() {
 
 		s.T().Log("waiting to see CNI pods ready")
 		s.NoError(common.WaitForKubeRouterReady(ctx, kc), "CNI did not start")
-	}); !ok {
-		return
-	}
 
-	s.Run("k0s reset", func() {
 		ssh, err := s.SSH(ctx, workerNode)
 		s.Require().NoError(err)
 		defer ssh.Disconnect()
@@ -57,16 +61,61 @@ func (s *suite) TestReset() {
 		s.NoError(ssh.Exec(ctx, "test -d /run/k0s", common.SSHStreams{}), "/run/k0s is not a directory")
 
 		s.NoError(ssh.Exec(ctx, "pidof containerd-shim-runc-v2 >&2", common.SSHStreams{}), "Expected some running containerd shims")
+	}) {
+		return
+	}
 
+	var clutteringPaths bytes.Buffer
+
+	if !s.Run("prepare k0s reset", func() {
 		s.NoError(s.StopWorker(workerNode), "Failed to stop k0s")
+
+		ssh, err := s.SSH(ctx, workerNode)
+		s.Require().NoError(err)
+		defer ssh.Disconnect()
+
+		streams, flushStreams := common.TestLogStreams(s.T(), "clutter data dir")
+		streams.In = bytes.NewReader(clutterScript)
+		streams.Out = io.MultiWriter(&clutteringPaths, streams.Out)
+		err = ssh.Exec(ctx, "sh -s -- /var/lib/k0s", streams)
+		flushStreams()
+		s.Require().NoError(err)
+	}) {
+		return
+	}
+
+	s.Run("k0s reset", func() {
+		ssh, err := s.SSH(ctx, workerNode)
+		s.Require().NoError(err)
+		defer ssh.Disconnect()
 
 		streams, flushStreams := common.TestLogStreams(s.T(), "reset")
 		err = ssh.Exec(ctx, "k0s reset --debug", streams)
 		flushStreams()
 		s.NoError(err, "k0s reset didn't exit cleanly")
 
-		// /var/lib/k0s is a mount point in the Docker container and can't be deleted, so it must be empty
-		s.NoError(ssh.Exec(ctx, `x="$(ls -A /var/lib/k0s)" && echo "$x" >&2 && [ -z "$x" ]`, common.SSHStreams{}), "/var/lib/k0s is not empty")
+		for _, path := range strings.Split(string(bytes.TrimSpace(clutteringPaths.Bytes())), "\n") {
+			if !strings.HasPrefix(path, "/var/lib/k0s/") {
+				s.NoError(ssh.Exec(ctx, fmt.Sprintf("test -e %q", path), common.SSHStreams{}), "Failed to verify existence of %s", path)
+			}
+		}
+
+		// Check that only the bind mounts are still there.
+		var remainingPaths strings.Builder
+		if s.NoError(ssh.Exec(ctx, "find /var/lib/k0s -mindepth 1 -maxdepth 3 -print0", common.SSHStreams{Out: &remainingPaths}), "failed to list contents of /var/lib/k0s") {
+			remainingPaths := strings.Split(strings.Trim(remainingPaths.String(), "\x00"), "\x00")
+			s.ElementsMatch(remainingPaths, []string{
+				"/var/lib/k0s/cluttered",
+				"/var/lib/k0s/cluttered/bind_dir",
+				"/var/lib/k0s/cluttered/bind_dir/in_overmount_dir.txt",
+				"/var/lib/k0s/cluttered/bind_file.txt",
+				"/var/lib/k0s/cluttered/rbind_dir",
+				"/var/lib/k0s/cluttered/rbind_dir/real_recursive_dir.txt",
+				"/var/lib/k0s/cluttered/rbind_dir/bind_dir",
+				"/var/lib/k0s/cluttered/rbind_dir/bind_file.txt",
+			})
+		}
+
 		s.NoError(ssh.Exec(ctx, "! test -e /run/k0s", common.SSHStreams{}), "/run/k0s still exists")
 		s.NoError(ssh.Exec(ctx, "! pidof containerd-shim-runc-v2 >&2", common.SSHStreams{}), "Expected no running containerd shims")
 	})
