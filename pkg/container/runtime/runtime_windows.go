fcbe580c2e6febc87f3433fadf1dd5d66accd437
Tom Wieczorek <twieczorek@mirantis.com>
2025-02-12 17:40:01 +0100

    XOXO(win-cri): Add support for named pipes
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/container/runtime/runtime_windows.go
+++ pkg/container/runtime/runtime_windows.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 k0s authors
+Copyright 2025 k0s authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,28 +17,27 @@ limitations under the License.
 package runtime
 
 import (
-	"context"
 	"net/url"
+	"path/filepath"
 
+	"github.com/Microsoft/go-winio"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type ContainerRuntime interface {
-	Ping(ctx context.Context) error
-	ListContainers(ctx context.Context) ([]string, error)
-	RemoveContainer(ctx context.Context, id string) error
-	StopContainer(ctx context.Context, id string) error
-}
-
-func NewContainerRuntime(runtimeEndpoint *url.URL) ContainerRuntime {
-	if rt := newPlatformSpecificContainerRuntime(runtimeEndpoint); rt != nil {
-		return rt
+func newPlatformSpecificContainerRuntime(runtimeEndpoint *url.URL) ContainerRuntime {
+	if runtimeEndpoint.Scheme != "npipe" {
+		return nil
 	}
 
 	return &CRIRuntime{
-		target: runtimeEndpoint.String(),
+		target: (&url.URL{
+			Scheme: "passthrough", // https://github.com/grpc/grpc-go/issues/7288#issuecomment-2141190333
+			Opaque: filepath.FromSlash(runtimeEndpoint.Path),
+		}).String(),
+
 		dialOptions: []grpc.DialOption{
+			grpc.WithContextDialer(winio.DialPipeContext),
 			grpc.WithTransportCredentials(insecure.NewCredentials()),
 		},
 	}
