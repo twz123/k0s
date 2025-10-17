twz123/k0s

commit 7986f1cf3fbbba354ffd529d8e0fb7e51fc30d52

Update dependency sigstore/cosign to v3
Signed-off-by: Renovate Bot <renovate@whitesourcesoftware.com>

hack/tools/Makefile.variables
@@ -3,4 +3,4 @@ controller-tools_version = 0.19.0
 # renovate: datasource=github-releases depName=golangci/golangci-lint
 golangci-lint_version = 2.5.0
 # renovate: datasource=github-releases depName=sigstore/cosign
-cosign_version = 2.6.1
+cosign_version = 3.0.2

commit 0839dfbb36011b7b5280fb1121a8ef838f5e429d

Always generate SBOM, but only sign it when releasing
This removes some additional workflows and workflow runs by simply
building the SBOM after a successful build. This takes only a couple of
seconds, since all the requirements are already there.

Signing, on the other hand, is only required for release artifacts.

This commit also includes a necessary fix for the cosign 3.x bump: It's
now required to pass the --use-signing-config=false flag.

Fixes: 7986f1cf3 ("Update dependency sigstore/cosign to v3")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

.github/workflows/build-k0s.yml
@@ -98,6 +98,15 @@ jobs:
           path: |
             k0s${{ steps.build-prepare.outputs.executable-suffix }}
 
+      - name: "Build :: SBOM"
+        run: make spdx.json
+
+      - name: "Upload :: SBOM"
+        uses: actions/upload-artifact@v4
+        with:
+          name: sbom-${{ inputs.target-os }}-${{ inputs.target-arch }}
+          path: spdx.json
+
       - name: "Build :: Airgap image list"
         if: inputs.target-os != 'windows'
         run: make airgap-images.txt && cat airgap-images.txt

.github/workflows/go.yml
@@ -134,26 +134,27 @@ jobs:
       target-os: linux
       target-arch: amd64
 
-  generate-sbom:
-    name: "Build :: SBOM"
-    needs: [build-k0s]
+  submit-sbom:
+    name: "Submit :: SBOM"
 
+    if: github.ref == 'refs/heads/main'
+    needs: [build-k0s]
     runs-on: ubuntu-24.04
 
+    permissions:
+      id-token: write
+      contents: write
+
     steps:
-      - name: Check out code into the Go module directory
-        uses: actions/checkout@v4
-
-      - name: Generate SBOM
-        run: |
-          make bindata
-          mkdir -p sbom && chmod 777 sbom
-          make sbom/spdx.json
-
-      - uses: actions/upload-artifact@v4
+      - name: Download
+        uses: actions/download-artifact@v5
         with:
-          name: spdx.json
-          path: sbom/spdx.json
+          name: sbom-linux-amd64
+
+      - name: Submit
+        uses: advanced-security/spdx-dependency-submission-action@v0.1.1
+        with:
+          filePath: spdx.json
 
   unittests-k0s:
     strategy:

.github/workflows/prepare-build-env.sh
@@ -4,13 +4,11 @@ set -eu
 
 goVersion="$(./vars.sh go_version)"
 golangciLintVersion="$(./vars.sh FROM=hack/tools golangci-lint_version)"
-cosignVersion="$(./vars.sh FROM=hack/tools cosign_version)"
 pythonVersion="$(./vars.sh FROM=docs python_version)"
 
 cat <<EOF >>"$GITHUB_ENV"
 GO_VERSION=$goVersion
 GOLANGCI_LINT_VERSION=$golangciLintVersion
-COSIGN_VERSION=$cosignVersion
 PYTHON_VERSION=$pythonVersion
 EOF
 

.github/workflows/release.yml
@@ -11,6 +11,7 @@ permissions:
 env:
   MAKEFLAGS: -j
   CURL_OPTS: --proto =https --tlsv1.2 --retry 5 --retry-all-errors --silent --show-error --location --fail
+  COSIGN_VERSION: "3.0.2" # renovate: datasource=github-releases depName=sigstore/cosign
 
 jobs:
   release:
@@ -74,7 +75,9 @@ jobs:
         run: .github/workflows/prepare-build-env.sh
 
       - name: Build
-        run: make EMBEDDED_BINS_BUILDMODE=docker
+        run: |
+          make EMBEDDED_BINS_BUILDMODE=docker
+          make spdx.json
         env:
           VERSION: ${{ needs.release.outputs.tag_name }}
 
@@ -98,15 +101,23 @@ jobs:
             /tmp/*.log
             /tmp/support-bundle.tar.gz
 
-      - name: Sign binary
+      - name: Sign binary and SBOM
         env:
           COSIGN_KEY: ${{ secrets.COSIGN_KEY }}
           COSIGN_PASSWORD: ${{ secrets.COSIGN_PASSWORD }}
         run: |
           curl $CURL_OPTS --output cosign https://github.com/sigstore/cosign/releases/download/v$COSIGN_VERSION/cosign-linux-amd64
           chmod +x ./cosign
-          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob --key env://COSIGN_KEY --tlog-upload -y --output-file=k0s.sig k0s
-          cat k0s.sig
+          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign public-key \
+            --key=env://COSIGN_KEY --output-file=cosign.pub
+          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob \
+            --key=env://COSIGN_KEY --use-signing-config=false \
+            --tlog-upload=true --yes \
+            --output-file=k0s.sig k0s
+          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob \
+            --key=env://COSIGN_KEY --use-signing-config=false \
+            --tlog-upload=true --yes \
+            --output-file=spdx.json.sig spdx.json
 
       - name: Upload Release Assets - Binary
         uses: shogo82148/actions-upload-release-asset@v1.8.2
@@ -130,6 +141,27 @@ jobs:
           upload_url: ${{ needs.release.outputs.upload_url }}
           asset_path: ./k0s.sig
           asset_name: k0s-${{ needs.release.outputs.tag_name }}-amd64.sig
+          asset_content_type: application/octet-stream
+
+      - name: Upload Release Assets - Public key
+        uses: shogo82148/actions-upload-release-asset@v1.8.2
+        with:
+          upload_url: ${{ needs.release.outputs.upload_url }}
+          asset_path: cosign.pub
+          asset_content_type: application/octet-stream
+
+      - name: Upload Release Assets - SBOM
+        uses: shogo82148/actions-upload-release-asset@v1.8.2
+        with:
+          upload_url: ${{ needs.release.outputs.upload_url }}
+          asset_path: spdx.json
+          asset_content_type: application/octet-stream
+
+      - name: Upload Release Assets - SBOM signature
+        uses: shogo82148/actions-upload-release-asset@v1.8.2
+        with:
+          upload_url: ${{ needs.release.outputs.upload_url }}
+          asset_path: spdx.json.sig
           asset_content_type: application/octet-stream
 
       - name: Upload Artifact for use in other Jobs
@@ -199,7 +231,10 @@ jobs:
         run: |
           curl $CURL_OPTS --output cosign https://github.com/sigstore/cosign/releases/download/v$COSIGN_VERSION/cosign-linux-amd64
           chmod +x ./cosign
-          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob --key env://COSIGN_KEY --tlog-upload -y --output-file=k0s.exe.sig k0s.exe
+          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob \
+            --key=env://COSIGN_KEY --use-signing-config=false \
+            --tlog-upload=true --yes \
+            --output-file=k0s.exe.sig k0s.exe
           cat k0s.exe.sig
 
       - name: Clean Docker
@@ -266,7 +301,10 @@ jobs:
         run: |
           curl $CURL_OPTS --output cosign https://github.com/sigstore/cosign/releases/download/v$COSIGN_VERSION/cosign-linux-arm64
           chmod +x ./cosign
-          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob --key env://COSIGN_KEY --tlog-upload -y --output-file=k0s.sig k0s
+          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob \
+            --key=env://COSIGN_KEY --use-signing-config=false \
+            --tlog-upload=true --yes \
+            --output-file=k0s.sig k0s
           cat k0s.sig
 
       - name: Set up Go for smoke tests
@@ -352,7 +390,10 @@ jobs:
         run: |
           curl $CURL_OPTS --output cosign https://github.com/sigstore/cosign/releases/download/v$COSIGN_VERSION/cosign-linux-arm
           chmod +x ./cosign
-          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob --key env://COSIGN_KEY --tlog-upload -y --output-file=k0s.sig k0s
+          COSIGN_KEY="$(printf %s "$COSIGN_KEY" | base64 -d)" ./cosign sign-blob \
+            --key=env://COSIGN_KEY --use-signing-config=false \
+            --tlog-upload=true --yes \
+            --output-file=k0s.sig k0s
           cat k0s.sig
 
       - name: Set up Go for smoke tests
@@ -523,31 +564,6 @@ jobs:
           build-args: |
             ALPINE_VERSION=${{ env.ALPINE_PATCH_VERSION }}
           push: true
-
-  sbom:
-    needs:
-      - release
-    runs-on: ubuntu-24.04
-    steps:
-      - name: Run git checkout
-        uses: actions/checkout@v4
-
-      - name: Generate SBOM
-        env:
-          COSIGN_KEY: ${{ secrets.COSIGN_KEY }}
-          COSIGN_PASSWORD: ${{ secrets.COSIGN_PASSWORD }}
-        run: |
-          mkdir -p sbom && chmod 777 sbom
-          echo $COSIGN_KEY | base64 -d > cosign.key
-          make sign-sbom
-          make sign-pub-key
-
-      - name: Upload Release Assets - SBOM
-        uses: shogo82148/actions-upload-release-asset@v1.8.2
-        with:
-          upload_url: ${{ needs.release.outputs.upload_url }}
-          asset_path: ./sbom/*
-          asset_content_type: application/octet-stream
 
   conformance-test:
     needs:

.github/workflows/sbom-upload.yml
@@ -1,40 +1,0 @@
-name: SBOM upload
-
-on:
-  workflow_dispatch:
-  push:
-    branches:
-      - main
-
-env:
-  MAKEFLAGS: -j
-
-jobs:
-  sbom-upload:
-    runs-on: ubuntu-24.04
-    permissions:
-      id-token: write
-      contents: write
-
-    steps:
-      - uses: actions/checkout@v4
-
-      - name: Generate SBOM
-        env:
-          COSIGN_KEY: ${{ secrets.COSIGN_KEY }}
-          COSIGN_PASSWORD: ${{ secrets.COSIGN_PASSWORD }}
-        run: |
-          make bindata
-          mkdir -p sbom && chmod 777 sbom
-          echo $COSIGN_KEY | base64 -d > cosign.key
-          make sign-sbom
-
-      - uses: actions/upload-artifact@v4
-        with:
-          name: sbom
-          path: sbom/
-
-      - name: SBOM upload
-        uses: advanced-security/spdx-dependency-submission-action@v0.1.1
-        with:
-          filePath: sbom/spdx.json

.gitignore
@@ -15,6 +15,7 @@
 /k0s.exe
 /k0s.exe.code
 /pkg/assets/zz_generated_offsets*.go
+/spdx.json
 .*.stamp
 *.tmp
 *.tmp.*

Makefile
@@ -325,6 +325,7 @@ clean: clean-gocache clean-docker-image clean-airgap-image-bundles
 	-find pkg/apis -type f -name .controller-gen.stamp -delete
 	-rm pkg/client/clientset/.client-gen.stamp
 	-rm -f hack/.copyright.stamp
+	-rm -f spdx.json
 	-$(MAKE) -C docs clean
 	-$(MAKE) -C embedded-bins clean
 	-$(MAKE) -C inttest clean
@@ -344,35 +345,12 @@ docs-serve-dev:
 	  -p '$(DOCS_DEV_PORT):8000' \
 	  $(DOCKER_RUN_OPTS) k0sdocs.docker-image.serve-dev
 
-sbom/spdx.json: go.mod
-	mkdir -p -- '$(dir $@)'
+spdx.json: syft.yaml go.mod .bins.$(TARGET_OS).stamp
 	$(DOCKER) run --rm \
-	  -v "$(CURDIR)/go.mod:/k0s/go.mod" \
-	  -v "$(CURDIR)/embedded-bins/staging/linux/bin:/k0s/bin" \
-	  -v "$(CURDIR)/syft.yaml:/tmp/syft.yaml" \
-	  -v "$(CURDIR)/sbom:/out" \
-	  --user $(BUILD_UID):$(BUILD_GID) \
-	  $(DOCKER_RUN_OPTS) anchore/syft:v0.90.0 \
-	  /k0s -o spdx-json@2.2=/out/spdx.json -c /tmp/syft.yaml
-
-.PHONY: sign-sbom
-sign-sbom: sbom/spdx.json
-	$(DOCKER) run --rm \
-	  -v "$(CURDIR):/k0s" \
-	  -v "$(CURDIR)/sbom:/out" \
-	  -e COSIGN_PASSWORD="$(COSIGN_PASSWORD)" \
-	  $(DOCKER_RUN_OPTS) ghcr.io/sigstore/cosign/cosign:v$(cosign_version) \
-	  sign-blob \
-	  --key /k0s/cosign.key \
-	  --tlog-upload=false \
-	  /k0s/sbom/spdx.json --output-file /out/spdx.json.sig
-
-.PHONY: sign-pub-key
-sign-pub-key:
-	$(DOCKER) run --rm \
-	  -v "$(CURDIR):/k0s" \
-	  -v "$(CURDIR)/sbom:/out" \
-	  -e COSIGN_PASSWORD="$(COSIGN_PASSWORD)" \
-	  $(DOCKER_RUN_OPTS) ghcr.io/sigstore/cosign/cosign:v$(cosign_version) \
-	  public-key \
-	  --key /k0s/cosign.key --output-file /out/cosign.pub
+	  -v '$(CURDIR)/syft.yaml':/k0s/syft.yaml:ro \
+	  -v '$(CURDIR)/go.mod':/k0s/go.mod:ro \
+	  -v '$(CURDIR)/bin':/k0s/embedded-bins/staging/$(TARGET_OS)/bin:ro \
+	  -w /k0s \
+	  $(DOCKER_RUN_OPTS) docker.io/anchore/syft:v1.24.0 \
+	  --source-name k0s --source-version '$(VERSION)' \
+	  -c syft.yaml -o spdx-json@2.2 . >'$@'

hack/tools/Makefile.variables
@@ -2,5 +2,3 @@
 controller-tools_version = 0.19.0
 # renovate: datasource=github-releases depName=golangci/golangci-lint
 golangci-lint_version = 2.5.0
-# renovate: datasource=github-releases depName=sigstore/cosign
-cosign_version = 3.0.2