twz123/k0s

commit 7b1c25bf68ce57d812e1cbd74a0bc4e75363a1ca

Apply the bootstraprbac stack directly
Don't write the manifests to disk, just use them directly in the Init
method as a standalone stack. Flag the stack name as ignored in the
applier manager.

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/component/controller/systemrbac.go
@@ -17,37 +17,75 @@ limitations under the License.
 package controller
 
 import (
+	"bytes"
+	"cmp"
 	"context"
 	_ "embed"
-	"path"
-	"path/filepath"
+	"fmt"
 
-	"github.com/k0sproject/k0s/internal/pkg/dir"
-	"github.com/k0sproject/k0s/internal/pkg/file"
+	"github.com/k0sproject/k0s/pkg/applier"
 	"github.com/k0sproject/k0s/pkg/component/manager"
 	"github.com/k0sproject/k0s/pkg/constant"
+	"github.com/k0sproject/k0s/pkg/kubernetes"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/cli-runtime/pkg/resource"
+
+	"github.com/avast/retry-go"
+	"github.com/sirupsen/logrus"
 )
+
+const SystemRBACStackName = "bootstraprbac"
 
 // SystemRBAC implements system RBAC reconciler
 type SystemRBAC struct {
-	manifestDir string
+	Clients kubernetes.ClientFactoryInterface
 }
 
 var _ manager.Component = (*SystemRBAC)(nil)
 
-// NewSystemRBAC creates new system level RBAC reconciler
-func NewSystemRBAC(manifestDir string) *SystemRBAC {
-	return &SystemRBAC{manifestDir}
-}
-
-// Writes the bootstrap RBAC manifests into the manifests folder.
-func (s *SystemRBAC) Init(context.Context) error {
-	rbacDir := path.Join(s.manifestDir, "bootstraprbac")
-	if err := dir.Init(rbacDir, constant.ManifestsDirMode); err != nil {
+// Applies the system RBAC manifests to the cluster.
+func (s *SystemRBAC) Init(ctx context.Context) error {
+	infos, err := resource.NewLocalBuilder().
+		Unstructured().
+		Stream(bytes.NewReader(systemRBAC), SystemRBACStackName).
+		Flatten().
+		Do().
+		Infos()
+	if err != nil {
 		return err
 	}
 
-	return file.WriteContentAtomically(filepath.Join(rbacDir, "bootstrap-rbac.yaml"), systemRBAC, 0644)
+	resources := make([]*unstructured.Unstructured, len(infos))
+	for i := range infos {
+		resources[i] = infos[i].Object.(*unstructured.Unstructured)
+	}
+
+	var lastErr error
+	if err := retry.Do(
+		func() error {
+			stack := applier.Stack{
+				Name:      SystemRBACStackName,
+				Resources: resources,
+				Clients:   s.Clients,
+			}
+			lastErr := stack.Apply(ctx, true)
+			return lastErr
+		},
+		retry.Context(ctx),
+		retry.LastErrorOnly(true),
+		retry.OnRetry(func(attempt uint, err error) {
+			logrus.WithFields(logrus.Fields{
+				"component": constant.SystemRBACComponentName,
+				"stack":     SystemRBACStackName,
+				"attempt":   attempt + 1,
+			}).WithError(err).Debug("Failed to apply stack, retrying after backoff")
+		}),
+	); err != nil {
+		return fmt.Errorf("failed to apply system RBAC stack: %w", cmp.Or(lastErr, err))
+	}
+
+	return nil
 }
 
 func (s *SystemRBAC) Start(context.Context) error { return nil }

cmd/controller/controller.go
@@ -323,6 +323,7 @@ func (c *command) start(ctx context.Context) error {
 			KubeClientFactory: adminClientFactory,
 			IgnoredStacks: []string{
 				controller.ClusterConfigStackName,
+				controller.SystemRBACStackName,
 			},
 			LeaderElector: leaderElector,
 		})
@@ -533,8 +534,8 @@ func (c *command) start(ctx context.Context) error {
 		clusterComponents.Add(ctx, reconciler)
 	}
 
-	if !slices.Contains(c.DisableComponents, constant.SystemRbacComponentName) {
-		clusterComponents.Add(ctx, controller.NewSystemRBAC(c.K0sVars.ManifestsDir))
+	if !slices.Contains(c.DisableComponents, constant.SystemRBACComponentName) {
+		clusterComponents.Add(ctx, &controller.SystemRBAC{Clients: adminClientFactory})
 	}
 
 	if !slices.Contains(c.DisableComponents, constant.NodeRoleComponentName) {

pkg/config/cli.go
@@ -262,7 +262,7 @@ var availableComponents = []string{
 	constant.MetricsServerComponentName,
 	constant.NetworkProviderComponentName,
 	constant.NodeRoleComponentName,
-	constant.SystemRbacComponentName,
+	constant.SystemRBACComponentName,
 	constant.WindowsNodeComponentName,
 	constant.WorkerConfigComponentName,
 }

pkg/constant/constant.go
@@ -118,7 +118,7 @@ const (
 	WorkerConfigComponentName          = "worker-config"
 	MetricsServerComponentName         = "metrics-server"
 	NetworkProviderComponentName       = "network-provider"
-	SystemRbacComponentName            = "system-rbac"
+	SystemRBACComponentName            = "system-rbac"
 	NodeRoleComponentName              = "node-role"
 	WindowsNodeComponentName           = "windows-node"
 	AutopilotComponentName             = "autopilot"

commit 6afa0f816918717b01c4b9ed8a84e2b00181b03a

Actually retain last error when applying the system RBAC
It was erroneously assigned to a local variable, instead to the one of
the outer scope.

Fixes: 7b1c25bf6 ("Apply the bootstraprbac stack directly")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

pkg/component/controller/systemrbac.go
@@ -63,7 +63,7 @@ func (s *SystemRBAC) Init(ctx context.Context) error {
 				Resources: resources,
 				Clients:   s.Clients,
 			}
-			lastErr := stack.Apply(ctx, true)
+			lastErr = stack.Apply(ctx, true)
 			return lastErr
 		},
 		retry.Context(ctx),