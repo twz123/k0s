55db9c8ae132595bbe5f5b753ff1fbed1a01d822
Tom Wieczorek <twieczorek@mirantis.com>
2026-03-06 15:01:41 +0100

    WIP internalos/watch
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- pkg/applier/stackapplier_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !linux
-
-// SPDX-FileCopyrightText: 2026 k0s authors
-// SPDX-License-Identifier: Apache-2.0
-
-package applier
-
-func handleCreateWatchErr(err error) (error, bool) { return err, false }
-func handleAddWatchErr(err error) (error, bool)    { return err, false }
