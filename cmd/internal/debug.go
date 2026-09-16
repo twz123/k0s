twz123/k0s

commit 0873cfe259cc133896ca73e4a8daf3b1b22dc3b5

Bump golangci-lint to v1.55.2
Adapt to updated linters:

* Update configuration
* Silence some opinionated new warnings

https://github.com/golangci/golangci-lint/releases/tag/v1.52.0
https://github.com/golangci/golangci-lint/releases/tag/v1.52.1
https://github.com/golangci/golangci-lint/releases/tag/v1.52.2
https://github.com/golangci/golangci-lint/releases/tag/v1.53.0
https://github.com/golangci/golangci-lint/releases/tag/v1.53.1
https://github.com/golangci/golangci-lint/releases/tag/v1.53.2
https://github.com/golangci/golangci-lint/releases/tag/v1.53.3
https://github.com/golangci/golangci-lint/releases/tag/v1.54.0
https://github.com/golangci/golangci-lint/releases/tag/v1.54.1
https://github.com/golangci/golangci-lint/releases/tag/v1.54.2
https://github.com/golangci/golangci-lint/releases/tag/v1.55.0
https://github.com/golangci/golangci-lint/releases/tag/v1.55.1
https://github.com/golangci/golangci-lint/releases/tag/v1.55.2

Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

.golangci.yml
@@ -25,16 +25,24 @@ linters:
 
 linters-settings:
   depguard:
-    packages:
-      - gopkg.in/yaml*
-    additional-guards:
-      # Only allow usages of the k8s cloud provider from within the k0s cloud
-      # provider package. This is to ensure that it's not leaking global flags
-      # into k0s.
-      - packages:
-          - k8s.io/cloud-provider*
-        ignore-file-rules:
-          - "**/pkg/k0scloudprovider/*.go"
+    rules:
+      yaml:
+        list-mode: lax
+        deny:
+          - pkg: gopkg.in/yaml.v2
+            desc: Use sigs.k8s.io/yaml.
+          - pkg: gopkg.in/yaml.v3
+            desc: Use sigs.k8s.io/yaml.
+      cloud-provider:
+        list-mode: lax
+        files:
+          - "!**/pkg/k0scloudprovider/*.go"
+        deny:
+          - pkg: k8s.io/cloud-provider
+            desc: >-
+              Usages of the k8s cloud provider are only allowed from within the
+              k0s cloud provider package. This is to ensure that it's not
+              leaking global flags into k0s.
   golint:
     min-confidence: 0
   goheader:
@@ -42,6 +50,12 @@ linters-settings:
     values:
       regexp:
         year: 202[0-9]
+
+  revive:
+    rules:
+      # This forbids to name variables "close", which seems natural for "close" functions.
+      - name: redefines-builtin-id
+        disabled: true
 
 issues:
   max-issues-per-linter: 0

hack/tools/Makefile.variables
@@ -1,3 +1,3 @@
 controller-gen_version = 0.13.0
 go-bindata_version = 3.23.0+incompatible
-golangci-lint_version = 1.51.2
+golangci-lint_version = 1.55.2

internal/pkg/sysinfo/probes/linux/cgroup_controllers.go
@@ -55,6 +55,7 @@ type cgroupControllerProbe struct {
 
 func (c *cgroupControllerProbe) Probe(reporter probes.Reporter) error {
 	desc := probes.NewProbeDesc(fmt.Sprintf("cgroup controller %q", c.name), c.path)
+	//revive:disable:indent-error-flow
 	if sys, err := c.probeSystem(); err != nil {
 		return reportCgroupSystemErr(reporter, desc, err)
 	} else if available, err := sys.probeController(c.name); err != nil {

internal/pkg/sysinfo/probes/linux/kernel.go
@@ -37,6 +37,7 @@ func (l *LinuxProbes) AssertKernelRelease(assert func(string) string) {
 	l.Set("kernelRelease", func(path probes.ProbePath, current probes.Probe) probes.Probe {
 		return probes.ProbeFn(func(r probes.Reporter) error {
 			desc := probes.NewProbeDesc("Linux kernel release", path)
+			//revive:disable:indent-error-flow
 			if uname, err := l.probeUname(); err != nil {
 				return r.Error(desc, err)
 			} else if uname.osRelease.truncated {

internal/pkg/sysinfo/probes/linux/linux.go
@@ -70,6 +70,7 @@ func (l *LinuxProbes) Probe(reporter probes.Reporter) error {
 
 func (l *LinuxProbes) probe(reporter probes.Reporter) error {
 	desc := probes.NewProbeDesc("Operating system", l.path)
+	//revive:disable:indent-error-flow
 	if uname, err := l.probeUname(); err != nil {
 		return reporter.Error(desc, err)
 	} else if uname.osName.value == "Linux" {

commit 8e8822d9debcda4790711feff2ca8ed73170e996

Re-enable revive's default rules
Turning them off was an oversight. It happened as a side effect of
adding the explicit deactivation of the "redefines-builtin-id" lint.
This resulted in revive not validating any rules. Listing any rules
without setting the enable-default-rules option turns off all the
default rules. Who would've expected that?

Enable the defaults and disable the ones that conflict with how the
code base is written. Fix most of the now uncovered warnings. Mark the
stuttering names with nolint directives for now. Renaming them can be
done separately later on.

Fixes: 0873cfe259c ("Bump golangci-lint to v1.55.2")
Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

.golangci.yml
@@ -101,10 +101,28 @@ linters:
         # Unclear whether it's a good idea for k0s to enforce this.
         - embedlit
     revive:
+      enable-default-rules: true # This is required as soon as individual rules get listed.
       rules:
+        # Doesn't take comments into account.
+        - name: empty-block
+          disabled: true
+        # Used intentionally to limit the scope of initialized variables.
+        - name: indent-error-flow
+          disabled: true
+        # Used intentionally to limit the scope of initialized variables.
+        - name: superfluous-else
+          disabled: true
         # This forbids to name variables "close", which seems natural for "close" functions.
         - name: redefines-builtin-id
           disabled: true
+        # Flags callback and interface method parameters, where the names serve as documentation.
+        # Removable parameters are already reported by unparam.
+        - name: unused-parameter
+          disabled: true
+        - name: var-naming
+          arguments:
+            # Mostly for C constants
+            - - upper-case-const: true
     staticcheck:
       checks:
         # Ignore deprecations: They shouldn't break the CI. If this were the

cmd/internal/debug.go
@@ -6,7 +6,7 @@ package internal
 import (
 	"errors"
 	"net/http"
-	_ "net/http/pprof"
+	_ "net/http/pprof" // registers the pprof handlers on the default serve mux
 	"os"
 	"strconv"
 

internal/pkg/dir/dir.go
@@ -12,7 +12,7 @@ import (
 	"github.com/opencontainers/selinux/go-selinux"
 )
 
-type DirOptions struct {
+type DirOptions struct { //nolint:revive // TODO rename to Options
 	path         string
 	perm         os.FileMode
 	seLinuxLabel string

pkg/airgap/ociartifactsbundler.go
@@ -70,7 +70,7 @@ func (b *OCIArtifactsBundler) Run(ctx context.Context, refs []reference.Named, o
 	} else {
 		b.Log.Infof("About to bundle %d artifacts", len)
 		var close func()
-		client, close = newHttpClient(b.InsecureRegistries == SkipTLSVerifyOCIRegistry)
+		client, close = newHTTPClient(b.InsecureRegistries == SkipTLSVerifyOCIRegistry)
 		defer close()
 	}
 
@@ -228,7 +228,7 @@ func (b *OCIArtifactsBundler) manifestsForRef(log *logrus.Entry, ref reference.N
 	return manifests, nil
 }
 
-func newHttpClient(insecureSkipTLSVerify bool) (_ *http.Client, close func()) {
+func newHTTPClient(insecureSkipTLSVerify bool) (_ *http.Client, close func()) {
 	// This transports is, by design, a trimmed down version of http's DefaultTransport.
 	// No need to have all those timeouts the default client brings in.
 	transport := &http.Transport{Proxy: http.ProxyFromEnvironment}
@@ -444,25 +444,25 @@ type ociLayoutArchiveWriter struct {
 	blobs []digest.Digest
 }
 
-func (t *ociLayoutArchive) doSynchronized(exclusive bool, fn func(w *ociLayoutArchiveWriter) error) (err error) {
+func (a *ociLayoutArchive) doSynchronized(exclusive bool, fn func(w *ociLayoutArchiveWriter) error) (err error) {
 	if exclusive {
-		t.mu.Lock()
+		a.mu.Lock()
 		defer func() {
 			if err != nil {
-				t.w = nil
+				a.w = nil
 			}
-			t.mu.Unlock()
+			a.mu.Unlock()
 		}()
 	} else {
-		t.mu.RLock()
-		defer t.mu.RUnlock()
+		a.mu.RLock()
+		defer a.mu.RUnlock()
 	}
 
-	if t.w == nil {
+	if a.w == nil {
 		return errors.New("writer is broken")
 	}
 
-	return fn(t.w)
+	return fn(a.w)
 }
 
 // Exists implements [oras.Target].

pkg/apis/k0s/v1beta1/telemetry.go
@@ -23,6 +23,6 @@ func DefaultClusterTelemetry() *ClusterTelemetry {
 }
 
 // Validate stub for Validateable interface
-func (c *ClusterTelemetry) Validate() []error {
+func (*ClusterTelemetry) Validate() []error {
 	return nil
 }

pkg/autopilot/client/client.go
@@ -27,7 +27,7 @@ type FactoryInterface interface {
 }
 
 // Deprecated: Use [kubeutil.ClientFactory] instead.
-type ClientFactory struct {
+type ClientFactory struct { //nolint:revive // deprecated
 	kubeutil.ClientFactoryInterface
 }
 

pkg/autopilot/controller/root/root.go
@@ -7,8 +7,7 @@ import (
 	"context"
 )
 
-// TODO: decide on renaming root.RootConfig -> root.Config
-type RootConfig struct {
+type RootConfig struct { //nolint:revive // TODO rename to Config
 	InvocationID        string
 	KubeConfig          string
 	K0sDataDir          string

pkg/autopilot/controller/setup.go
@@ -58,14 +58,14 @@ func (c *rootController) setup(ctx context.Context) error {
 
 // createControlNode creates a new control node, ignoring errors if one already exists
 // for this physical host.
-func (sc *rootController) createControlNode(ctx context.Context, name, nodeName string) error {
-	if !sc.apiAddress.IsValid() {
+func (c *rootController) createControlNode(ctx context.Context, name, nodeName string) error {
+	if !c.apiAddress.IsValid() {
 		return errors.New("no API address given")
 	}
-	apiAddress := sc.apiAddress.String()
+	apiAddress := c.apiAddress.String()
 
-	logger := sc.log.WithField("component", "setup")
-	client, err := sc.kubeClientFactory.GetK0sClient()
+	logger := c.log.WithField("component", "setup")
+	client, err := c.kubeClientFactory.GetK0sClient()
 	if err != nil {
 		return err
 	}
@@ -74,7 +74,7 @@ func (sc *rootController) createControlNode(ctx context.Context, name, nodeName 
 	node, err := client.AutopilotV1beta2().ControlNodes().Get(ctx, name, metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
 		logger.Info("Autopilot 'controlnodes' CRD not found, waiting...")
-		if err := sc.waitForControlNodesCRD(ctx); err != nil {
+		if err := c.waitForControlNodesCRD(ctx); err != nil {
 			return fmt.Errorf("while waiting for autopilot 'controlnodes' CRD: %w", err)
 		}
 
@@ -82,7 +82,7 @@ func (sc *rootController) createControlNode(ctx context.Context, name, nodeName 
 
 		logger.Infof("ControlNode '%s' not found, creating", name)
 		mode := apconst.K0SControlNodeModeController
-		if sc.enableWorker {
+		if c.enableWorker {
 			mode = apconst.K0SControlNodeModeControllerWorker
 		}
 		node = &apv1beta2.ControlNode{
@@ -129,8 +129,8 @@ func (sc *rootController) createControlNode(ctx context.Context, name, nodeName 
 
 // waitForControlNodesCRD waits until the controlnodes CRD is established for
 // max 2 minutes.
-func (sc *rootController) waitForControlNodesCRD(ctx context.Context) error {
-	extClient, err := sc.kubeClientFactory.GetAPIExtensionsClient()
+func (c *rootController) waitForControlNodesCRD(ctx context.Context) error {
+	extClient, err := c.kubeClientFactory.GetAPIExtensionsClient()
 	if err != nil {
 		return fmt.Errorf("unable to obtain extensions client: %w", err)
 	}
@@ -141,7 +141,7 @@ func (sc *rootController) waitForControlNodesCRD(ctx context.Context) error {
 		WithObjectName("controlnodes."+apv1beta2.GroupName).
 		WithErrorCallback(func(err error) (time.Duration, error) {
 			if retryDelay, e := watch.IsRetryable(err); e == nil {
-				sc.log.WithError(err).Debugf(
+				c.log.WithError(err).Debugf(
 					"Encountered transient error while waiting for autopilot 'controlnodes' CRD, retrying in %s",
 					retryDelay,
 				)

pkg/backup/sqlitedb_unix.go
@@ -13,7 +13,7 @@ import (
 	"path/filepath"
 
 	"github.com/sirupsen/logrus"
-	_ "modernc.org/sqlite"
+	_ "modernc.org/sqlite" // registers the sqlite database/sql driver
 
 	"github.com/k0sproject/k0s/internal/pkg/dir"
 	"github.com/k0sproject/k0s/internal/pkg/file"

pkg/component/controller/cplb/cplb_reconciler.go
@@ -30,7 +30,7 @@ import (
 // CPLBReconciler monitors the endpoints of the "kubernetes" service in the
 // "default" namespace. It notifies changes though the updateCh channel provided
 // in the constructor.
-type CPLBReconciler struct {
+type CPLBReconciler struct { //nolint:revive // TODO rename to Reconciler
 	apiPort        int
 	log            *logrus.Entry
 	kubeconfigPath string

pkg/install/service.go
@@ -57,6 +57,8 @@ func InstalledService() (service.Service, error) {
 }
 
 // InstallService installs the k0s service, per the given arguments, and the detected platform
+//
+//nolint:revive // TODO figure out a new name
 func InstallService(ctx context.Context, args []string, envVars []string, force bool) error {
 	var svcConfig *service.Config
 

static/assets.go
@@ -12,19 +12,19 @@ import (
 var (
 	//go:embed _crds
 	crds embed.FS
-	CRDs fs.FS = subFS(crds, "_crds")
+	CRDs = subFS(crds, "_crds")
 )
 
 var (
 	//go:embed manifests/calico
 	calicoManifests embed.FS
-	CalicoManifests fs.FS = subFS(calicoManifests, "manifests", "calico")
+	CalicoManifests = subFS(calicoManifests, "manifests", "calico")
 )
 
 var (
 	//go:embed manifests/windows
 	windowsManifests embed.FS
-	WindowsManifests fs.FS = subFS(windowsManifests, "manifests", "windows")
+	WindowsManifests = subFS(windowsManifests, "manifests", "windows")
 )
 
 func subFS(f fs.FS, segments ...string) fs.FS {

pkg/autopilot/controller/plans/cmdprovider/k0supdate/utils/find_test.go
@@ -34,7 +34,7 @@ func TestFindNextPendingRandom(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NotNil(t, node)
 
-		countMap[node.Name] += 1
+		countMap[node.Name]++
 	}
 
 	assert.Contains(t, countMap, "bbb")

pkg/component/controller/cplb/tcpproxy/tcpproxy_test.go
@@ -40,7 +40,6 @@ func testListenFunc(t *testing.T, ln net.Listener) func(network, laddr string) (
 		}
 		if laddr != testFrontAddr {
 			t.Fatalf("got Listen call with laddr %q, want %q", laddr, testFrontAddr)
-			panic("bogus address")
 		}
 		return ln, nil
 	}